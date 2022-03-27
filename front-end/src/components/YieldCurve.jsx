ebd83dae41e2c91c0c34f40b55c36bea0a63a452
BurntLeaves <burnt_leaf@ctemplar.com>
2022-03-27 12:10:58 +0100

    fix: fixed json error

--- front-end/src/components/YieldCurve.jsx
+++ front-end/src/components/YieldCurve.jsx
@@ -9,7 +9,7 @@ const YieldCurveComp = () => {
     asyncFetch()
   }, [])
 
-  json = setData(json)
+  // TODO: Implement data from rates or test_rates
 
   const asyncFetch = () => {
     fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
